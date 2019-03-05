b91be66c5cb747e05910cb2cb77b8ba06d73f2cd
Princeton Rose <decoyzpr@gmail.com>
2019-03-05 03:57:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,5 +65,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbq = []
+  holiday_hash.each do |season, holiday|
+    if holiday.include?("BBQ")
+      bbq.push(holiday)
+    end
+    bbq
 end
